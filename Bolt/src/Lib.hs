0abcef500280798600faabc099907e50918929d4
dinnu93 <dinesh.padisetti@gmail.com>
2016-05-26 16:47:59 +0530

    Completed json encoder

--- Bolt/src/Lib.hs
+++ Bolt/src/Lib.hs
@@ -1,29 +1,33 @@
 module Lib where
 
+import Data.String.Utils
+import Data.Char
+import qualified Data.Map as Map
 -- Writing the definitions of each data type in the JSON specification
 -- document(ECMA-404) in terms of the Bolt values.
 
 
-data BValue = BObject [(String, BValue)]| -- can be identified with {}
+data BValue = BObject (Map.Map String BValue)| -- can be identified with {}
               BArray [BValue]| -- can be identified with []
-              BNumber Integer| 
+              BNumber Double| 
               BString String|
               BBool Bool|
               BNull deriving (Show)
 
 -- Encoding a JSON string into the BValue algebraic data type.
 
-type JSON = String
 
--- encodeJSON :: JSON -> BValue
--- encodeJSON "true" = BBool True
--- encodeJSON "false" = BBool False
--- encodeJSON "null" = BNull
--- encodeJSON s
---   | isJSONString s = parseJSONString s
---   | isJSONArray s = parseJSONArray s
---   | isJSONObject s = parseJSONObject s
---   | otherwise = "Not a valid JSON String!"
+
+encodeJSON :: String -> BValue
+encodeJSON "true" = BBool True
+encodeJSON "false" = BBool False
+encodeJSON "null" = BNull
+encodeJSON s
+  | isJSONNumber s = parseJSONNumber s
+  | isJSONString s = parseJSONString s
+  | isJSONArray s = parseJSONArray s
+  | isJSONObject s = parseJSONObject s
+  | otherwise = error "Not a valid JSON String!"
   
 
 -- Predicates for JSON data types
@@ -33,21 +37,40 @@ encapsulatedIn :: Char -> Char -> String -> Bool
 encapsulatedIn c1 c2 s
   | (length s > 1) && (head s) == c1 && (last s) == c2 = True
   | otherwise = False 
-    
-    
-isJSONString :: JSON -> Bool
-isJSONString = encapsulatedIn '"' '"'
 
-isJSONArray :: JSON -> Bool
-isJSONArray = encapsulatedIn '[' ']'
+strippedString :: String -> String
+strippedString s = drop 1 . take (l-1) $ stripStr
+  where stripStr = strip s
+        l = length stripStr
+    
+isJSONString :: String -> Bool
+isJSONString = encapsulatedIn '"' '"' . strip 
 
-isJSONObject :: JSON -> Bool
-isJSONObject = encapsulatedIn '{' '}'
+isJSONNumber :: String -> Bool
+isJSONNumber = and . map isNumber . strip   
+
+isJSONArray :: String -> Bool
+isJSONArray = encapsulatedIn '[' ']' . strip 
+
+isJSONObject :: String -> Bool
+isJSONObject = encapsulatedIn '{' '}' . strip 
 
 -- parsing out JSON data types to Bolt's data types
 
-parseJSONString :: JSON -> BValue
-parseJSONString s = BString (drop 1 . take (l-1) $ s) 
-  where l = length s
-     
+parseJSONString :: String -> BValue
+parseJSONString s = BString $ strippedString s
   
+parseJSONNumber :: String -> BValue
+parseJSONNumber s = BNumber (read $ strip s :: Double) 
+
+parseJSONArray :: String -> BValue
+parseJSONArray s = BArray (map encodeJSON . split "," . strippedString $ s)
+
+parseJSONPair :: String -> (String, BValue)
+parseJSONPair s = (strippedString key, encodeJSON value)
+  where [key,value] = map strip . split ":" . strip $ s
+
+parseJSONObject :: String -> BValue
+parseJSONObject s = BObject (Map.fromList . map parseJSONPair . split "," . strippedString $ s)
+
+
